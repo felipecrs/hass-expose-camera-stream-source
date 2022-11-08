90adb540a592c49f3ff05bed5c40dc9be2e14cef
Felipe Santos <felipecassiors@gmail.com>
2022-11-08 14:23:16 +0000

    Fix tests

--- tests/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for expose_camera_stream_source tests."""
-from custom_components.expose_camera_stream_source.const import (
-    CONF_PASSWORD,
-    CONF_USERNAME,
-)
-
-# Mock config data to be used across multiple tests
-MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
